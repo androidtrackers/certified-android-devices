e56018149dd40ed80ad4a0209d31663a4e8714ed
yshalsager <ysh-alsager@hotmail.com>
2018-04-18 15:19:12 +0200

    Enable Travis Weekly

--- sync.sh
+++ sync.sh
@@ -5,13 +5,11 @@ csv2md devices.csv > README.md
 today=$(date +%d.%m.%Y)
 cat << EOF >> head
 # Google Play Certified Android devices
-
 Last sync is $today
-
 https://support.google.com/googleplay/answer/1727131?hl=en
-
 EOF
-cat head README.md >temp && mv temp README.md
-git config --global user.email "$gitmail" ; git config --global user.name "$gituser"
-git add README.md ; git commit -m "$(date +%d.%m.%Y)"
-git push -q https://$GIT_OAUTH_TOKEN_XFU@github.com/yshalsager/certified-android-devices.git master
+cat head README.md > temp && mv temp README.md
+sudo date -s '+2 hour'
+git config --global user.email "$gitmail"; git config --global user.name "$gituser"
+git add README.md; git commit -m "$(date +%d.%m.%Y)"; git log
+git push -q https://$GIT_OAUTH_TOKEN_XFU@github.com/yshalsager/certified-android-devices.git HEAD:master
