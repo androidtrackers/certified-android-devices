5dc6b23a78709bc0b44d037132a4d5de15103d3e
yshalsager <ysh-alsager@hotmail.com>
2025-03-28 14:43:18 +0200

    style: format

--- sync.py
+++ sync.py
@@ -5,14 +5,15 @@ import difflib
 import json
 import sys
 from datetime import date
-from os import rename, path, system, environ
+from os import environ, path, rename, system
 from time import sleep
+
 from requests import get, post
 
-GIT_OAUTH_TOKEN = environ.get('GIT_OAUTH_TOKEN_XFU', '')
-BOT_TOKEN = environ.get('BOTTOKEN', '')
+GIT_OAUTH_TOKEN = environ.get("GIT_OAUTH_TOKEN_XFU", "")
+BOT_TOKEN = environ.get("BOTTOKEN", "")
 
-LOCAL_MODE = '--local' in sys.argv or not GIT_OAUTH_TOKEN or not BOT_TOKEN
+LOCAL_MODE = "--local" in sys.argv or not GIT_OAUTH_TOKEN or not BOT_TOKEN
 if LOCAL_MODE:
     print("Running in local mode - no GitHub or Telegram updates will be performed")
 
@@ -26,20 +27,22 @@ BY_NAME = {}
 
 def save_data(data_list):
     """Save Data to various files"""
-    markdown = open('README.md', 'w', encoding="utf-8")
-    markdown.write('# Google Play Certified Android devices\n')
-    markdown.write('Last sync is {}\n\nhttps://support.google.com/googleplay/'
-                   'answer/1727131?hl=en\n\n'.format(TODAY))
-    markdown.write('|Retail Branding|Marketing Name|Device|Model|\n')
-    markdown.write('|---|---|---|---|\n')
+    markdown = open("README.md", "w", encoding="utf-8")
+    markdown.write("# Google Play Certified Android devices\n")
+    markdown.write(
+        "Last sync is {}\n\nhttps://support.google.com/googleplay/"
+        "answer/1727131?hl=en\n\n".format(TODAY)
+    )
+    markdown.write("|Retail Branding|Marketing Name|Device|Model|\n")
+    markdown.write("|---|---|---|---|\n")
     for line in data_list[1:]:
-        i = line.strip().replace('"', '').split(",")
+        i = line.strip().replace('"', "").split(",")
         try:
             brand = i[0].strip()
             name = i[1].strip()
             device = i[2].strip()
             model = i[3].strip()
-            markdown.write('|{}|{}|{}|{}|\n'.format(brand, name, device, model))
+            markdown.write("|{}|{}|{}|{}|\n".format(brand, name, device, model))
             add_device(brand, name, device, model)
             add_model(brand, name, device, model)
             add_brand(brand, name, device, model)
@@ -59,53 +62,37 @@ def save_data(data_list):
 def add_device(brand, name, device, model):
     """add device to devices dict"""
     try:
-        updated = BY_DEVICE[device] + [{'brand': brand, 'name': name, 'model': model}]
-        BY_DEVICE.update({
-            device: updated
-        })
+        updated = BY_DEVICE[device] + [{"brand": brand, "name": name, "model": model}]
+        BY_DEVICE.update({device: updated})
     except KeyError:
-        BY_DEVICE.update({
-            device: [{'brand': brand, 'name': name, 'model': model}]
-        })
+        BY_DEVICE.update({device: [{"brand": brand, "name": name, "model": model}]})
 
 
 def add_model(brand, name, device, model):
     """add device to models dict"""
     try:
-        updated = BY_MODEL[model] + [{'brand': brand, 'name': name, 'device': device}]
-        BY_MODEL.update({
-            model: updated
-        })
+        updated = BY_MODEL[model] + [{"brand": brand, "name": name, "device": device}]
+        BY_MODEL.update({model: updated})
     except KeyError:
-        BY_MODEL.update({
-            model: [{'brand': brand, 'name': name, 'device': device}]
-        })
+        BY_MODEL.update({model: [{"brand": brand, "name": name, "device": device}]})
 
 
 def add_brand(brand, name, device, model):
     """add device to brand dict"""
     try:
-        updated = BY_BRAND[brand] + [{'device': device, 'name': name, 'model': model}]
-        BY_BRAND.update({
-            brand: updated
-        })
+        updated = BY_BRAND[brand] + [{"device": device, "name": name, "model": model}]
+        BY_BRAND.update({brand: updated})
     except KeyError:
-        BY_BRAND.update({
-            brand: [{'device': device, 'name': name, 'model': model}]
-        })
+        BY_BRAND.update({brand: [{"device": device, "name": name, "model": model}]})
 
 
 def add_name(brand, name, device, model):
     """add device to names dict"""
     try:
-        updated = BY_NAME[name] + [{'brand': brand, 'device': device, 'model': model}]
-        BY_NAME.update({
-            name: updated
-        })
+        updated = BY_NAME[name] + [{"brand": brand, "device": device, "model": model}]
+        BY_NAME.update({name: updated})
     except KeyError:
-        BY_NAME.update({
-            name: [{'brand': brand, 'device': device, 'model': model}]
-        })
+        BY_NAME.update({name: [{"brand": brand, "device": device, "model": model}]})
 
 
 def fetch():
@@ -114,8 +101,8 @@ def fetch():
     """
     url = "http://storage.googleapis.com/play_public/supported_devices.csv"
     response = get(url)
-    data = (response.content.decode('utf-16'))
-    data_list = list(data.split('\n'))
+    data = response.content.decode("utf-16")
+    data_list = list(data.split("\n"))
     return data_list
 
 
@@ -123,14 +110,16 @@ def diff_files():
     """
     diff
     """
-    with open('old.md', 'r') as old, open('README.md', 'r') as new:
-        diff = difflib.unified_diff(old.readlines(), new.readlines(), fromfile='old', tofile='new')
+    with open("old.md", "r") as old, open("README.md", "r") as new:
+        diff = difflib.unified_diff(
+            old.readlines(), new.readlines(), fromfile="old", tofile="new"
+        )
         changes = []
         for line in diff:
-            if line.startswith('+'):
+            if line.startswith("+"):
                 changes.append(str(line))
-    new = ''.join(changes[2:]).replace("+", "")
-    with open('changes', 'w') as out:
+    new = "".join(changes[2:]).replace("+", "")
+    with open("changes", "w") as out:
         out.write(new)
 
 
@@ -140,23 +129,25 @@ def post_to_tg():
     """
     # tg
     telegram_chat = "@CertifiedAndroidDevices"
-    with open('changes', 'r') as changes:
+    with open("changes", "r") as changes:
         for line in changes:
             info = line.split("|")
             brand = info[1]
             name = info[2]
             codename = info[3]
             model = info[4]
-            telegram_message = f"New certified device added!: \n" \
-                               f"Brand: *{brand}*\n" \
-                               f"Name: *{name}*\n" \
-                               f"*Codename:* `{codename}`\n" \
-                               f"Model: *{model}*"
+            telegram_message = (
+                f"New certified device added!: \n"
+                f"Brand: *{brand}*\n"
+                f"Name: *{name}*\n"
+                f"*Codename:* `{codename}`\n"
+                f"Model: *{model}*"
+            )
             params = (
-                ('chat_id', telegram_chat),
-                ('text', telegram_message),
-                ('parse_mode', "Markdown"),
-                ('disable_web_page_preview', "yes")
+                ("chat_id", telegram_chat),
+                ("text", telegram_message),
+                ("parse_mode", "Markdown"),
+                ("disable_web_page_preview", "yes"),
             )
             telegram_url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage"
             telegram_req = post(telegram_url, params=params)
@@ -172,20 +163,23 @@ def git_commit_push():
     """
     git add - git commit - git push
     """
-    system("git add README.md *.json && git -c \"user.name=XiaomiFirmwareUpdater\" "
-           "-c \"user.email=xiaomifirmwareupdater@gmail.com\" "
-           "commit -m \"[skip ci] sync: {0}\" && "" \
-           ""git push -q https://{1}@github.com/androidtrackers/"
-           "certified-android-devices.git HEAD:master"
-           .format(TODAY, GIT_OAUTH_TOKEN))
+    system(
+        'git add README.md *.json && git -c "user.name=XiaomiFirmwareUpdater" '
+        '-c "user.email=xiaomifirmwareupdater@gmail.com" '
+        'commit -m "[skip ci] sync: {0}" && '
+        " \
+           "
+        "git push -q https://{1}@github.com/androidtrackers/"
+        "certified-android-devices.git HEAD:master".format(TODAY, GIT_OAUTH_TOKEN)
+    )
 
 
 def main():
     """
     certified-android-devices tracker
     """
-    if path.exists('README.md'):
-        rename('README.md', 'old.md')
+    if path.exists("README.md"):
+        rename("README.md", "old.md")
     data_list = fetch()
     save_data(data_list)
     diff_files()
@@ -194,5 +188,5 @@ def main():
         git_commit_push()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
